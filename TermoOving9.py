db3107ce3b1e6cd6610bbec2dc79d80a2382ab5d
Markus Hatlo <marhat182@gmail.com>
2025-03-13 22:54:57 +0100

    ferdig med oving 9

--- TermoOving9.py
+++ TermoOving9.py
@@ -1,4 +1,6 @@
 import sympy as sp
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Task 1
 print('Task 1')
@@ -29,5 +31,40 @@ v_mol_rk = v_molar_rk[0]
 v_spesific_rk = v_mol_rk/18
 print('v_spesific using redlich-kwong:', v_spesific_rk)
 
-# Task 2
-print('\nTask 2')
+#Task 3
+print('\nTask 3')
+
+R_spes = 8.314e3/44.01
+a_Walls_CO2 = 3.647e5/44.01**2
+b_Walls_CO2 = 0.0428/44.01**2
+a_Redlich_CO2 = 64.43e5/44.01**2
+b_Redlich_CO2 = 0.02963/44.01**2
+T3 = 240
+
+def calcIdealGas(v):
+    return (R_spes*T3/v)/1e5
+
+def calcVanDerWaals(v):
+    return ((R_spes*T3/(v-b_Walls_CO2)) - (a_Walls_CO2/v**2))/1e5
+
+def calcRedlichKwong(v):
+    return ((R_spes*T3/(v-b_Redlich_CO2)) - a_Redlich_CO2/(v*(v+b_Redlich_CO2)*T3**0.5))/1e5
+
+
+
+v_plot = np.linspace(1e-4,0.05,100)
+p_Ideal_plot = calcIdealGas(v_plot)
+p_VDW_plot = calcVanDerWaals(v_plot)
+p_RK_plot = calcRedlichKwong(v_plot)
+
+plt.plot(v_plot, p_Ideal_plot)
+plt.plot(v_plot, p_VDW_plot)
+plt.plot(v_plot, p_RK_plot)
+plt.legend(['Ideal gas', 'Van der Waals', 'Redlich-Kwong'])
+plt.ylim(0, 50)
+#plt.xlim(0.01, 0.05)
+plt.xlabel('Volume [m^3/mol]')
+plt.ylabel('Pressure [Bar]')
+plt.title('Ideal gas')
+plt.show()
+
